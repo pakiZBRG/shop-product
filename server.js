40701c0cea2932775558d0a0d5bc49b61306f88d
unknown <pavlovicnikola511@gmail.com>
2020-09-15 18:12:30 +0200

    heroku deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require('path');
 const db = require("./config/keys").mongoURI;
 const items = require("./routes/items");
 const app = express();
@@ -12,6 +13,14 @@ mongoose.connect(db, {useNewUrlParser: true,useUnifiedTopology: true,})
 
 //Use Routes
 app.use("/api/items", items);
+
+//If in Production
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server connected on port ${port}`));
